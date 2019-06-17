4540ca243e2e2b020e9b2f6f78910d56f4dfa81c
Nguyen Manh Hung <hung.nguyen@med2lab.com>
2019-06-17 21:41:46 +0700

    Builder pattern: Separate the construction of a complex object from its representation so that the same construction process can create different representations.

---
+++ Builder/Structural.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    // This structural code demonstrates the Builder pattern in which complex objects are created in a step-by-step fashion. 
+    // The construction process can create different object representations and provides a high level of control over the assembly of the objects.
+    public class Structural
+    {
+        public void Run()
+        {
+
+            // Create director and builders
+            Director director = new Director();
+            Builder b1 = new ConcreteBuilder1();
+            Builder b2 = new ConcreteBuilder2();
+            // Construct two products
+            director.Construct(b1);
+            Product p1 = b1.GetResult();
+            p1.Show();
+            director.Construct(b2);
+            Product p2 = b2.GetResult();
+            p2.Show();
+        }
+    }
+    /// <summary>
+    /// The 'Director' class
+    /// </summary>
+    class Director
+    {
+        // Builder uses a complex series of steps
+        public void Construct(Builder builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    }
+
+    /// <summary>
+    /// The 'Builder' abstract class
+    /// </summary>
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract Product GetResult();
+    }
+
+    /// <summary>
+    /// The 'ConcreteBuilder1' class
+    /// </summary>
+    class ConcreteBuilder1 : Builder
+    {
+        private readonly Product _product = new Product();
+        public override void BuildPartA()
+        {
+            _product.Add("PartA");
+        }
+        public override void BuildPartB()
+        {
+            _product.Add("PartB");
+        }
+        public override Product GetResult()
+        {
+            return _product;
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteBuilder2' class
+    /// </summary>
+    class ConcreteBuilder2 : Builder
+    {
+        private readonly Product _product = new Product();
+        public override void BuildPartA()
+        {
+            _product.Add("PartX");
+        }
+        public override void BuildPartB()
+        {
+            _product.Add("PartY");
+        }
+
+        public override Product GetResult()
+        {
+            return _product;
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' class
+    /// </summary>
+    class Product
+    {
+        private readonly List<string> _parts = new List<string>();
+        public void Add(string part)
+        {
+            _parts.Add(part);
+        }
+        public void Show()
+        {
+            Console.WriteLine("\nProduct Parts -------");
+            foreach (string part in _parts)
+                Console.WriteLine(part);
+        }
+    }
+}
